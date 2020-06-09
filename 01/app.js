c34762ae3f310ec32edeacc7dc0b795ebdbccaf7
Martyna Kiljan <kmartynak@icloud.com>
2020-06-09 16:36:40 +0200

    objekty_1

---
+++ 01/app.js
@@ -0,0 +1,14 @@
+const user = {
+    firstName: 'Martyna',
+    lastName: 'Kiljan',
+    sex: 'women',
+    age: 25,
+}
+
+console.log(user)
+//albo//
+
+console.log(user.firstName)
+console.log(user.lastName)
+console.log(user.sex)
+console.log(user.age)

a12bbf4f762c591083b7d36ab31750b94ce1399d
Martyna Kiljan <kmartynak@icloud.com>
2020-06-10 16:33:19 +0200

    04_objekty

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,32 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '13',
+        month: '6',
         year: '1985'
     }
 }
+
+
+
+const checkBirthday = function (born) {
+    const date = new Date();
+
+    // console.log(date.getFullYear(),
+    //     date.getMonth() + 1,
+    //     date.getDate())
+
+    let month = date.getMonth() + 1;
+    console.log(month)
+    let day = date.getDate();
+    // console.log(day)
+
+    if (month == born.month && day == born.day) {
+        console.log('masz dziś urodziny!')
+    } else {
+        console.log('do twoich urodzin jeszcze trochę!')
+    }
+
+}
+
+checkBirthday(user.born)

5c5fd7fb47276349980b8afb69d22a4668c8d936
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-03 11:41:22 +0200

    task 4 done

--- 04/app.js
+++ 04/app.js
@@ -7,3 +7,11 @@ const user = {
         year: '1985'
     }
 }
+const day = new Date().getUTCDate();
+const month = new Date().getMonth();
+console.log(month);
+if (user.born.day == day && user.born.month == month) {
+    console.log('Happy B-Day')
+} else {
+    console.log('Not your birthday today')
+}

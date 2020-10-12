0b7cd51ada51afde68d32d6cbf1de7c2210a2505
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-12 15:14:06 +0200

    Check is user has birthday

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,23 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
-        year: '1985'
+        day: '12',
+        month: '10',
+        year: '2020'
+    }
+}
+
+user.checkBirthday = function(){
+    const currYear = (new Date()).getFullYear();
+    const currMonth = (new Date()).getMonth() +1;
+    const currDay = (new Date()).getDate();
+    console.log(currYear);
+    console.log(currMonth);
+    console.log(currDay);
+
+    if(currYear==this.born.year && currMonth==this.born.month &&currDay==this.born.day){
+        return 'Wszystkiego najlepszego!!! :-)'
+    } else{
+        return 'Użytkownik nie ma dziś urodzin'
     }
 }

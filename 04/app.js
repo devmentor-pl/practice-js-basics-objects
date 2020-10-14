097800188d9a2a3d79894e02f6b98b1f7c6a2324
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-14 17:33:41 +0200

    Fix Task 04

--- 04/app.js
+++ 04/app.js
@@ -2,23 +2,23 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '12',
+        day: '14',
         month: '10',
         year: '2020'
     }
 }
 
-user.checkBirthday = function(){
+user.checkBirthday = function () {
     const currYear = (new Date()).getFullYear();
-    const currMonth = (new Date()).getMonth() +1;
+    const currMonth = (new Date()).getMonth() + 1;
     const currDay = (new Date()).getDate();
     console.log(currYear);
     console.log(currMonth);
     console.log(currDay);
 
-    if(currYear==this.born.year && currMonth==this.born.month &&currDay==this.born.day){
+    if (currMonth == this.born.month && currDay == this.born.day) {
         return 'Wszystkiego najlepszego!!! :-)'
-    } else{
+    } else {
         return 'Użytkownik nie ma dziś urodzin'
     }
 }

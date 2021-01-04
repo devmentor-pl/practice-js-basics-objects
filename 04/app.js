c812d947afb2975b4d641c2cefeaa3b6dce012bf
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-04 02:27:01 +0100

    Zad 04

--- 04/app.js
+++ 04/app.js
@@ -7,3 +7,21 @@ const user = {
         year: '1985'
     }
 }
+
+user.checkingDate = function (day, month) {
+
+    const currentMonth = new Date().getMonth();
+    const userMonth = this['born']['month'];
+
+    if (userMonth === currentMonth) {
+
+        const currentDay = new Date().getDate();
+        const userDay = this['born']['day'];
+
+        if (currentDay === userDay) {
+            const congrats = 'Sto lat';
+            return congrats;
+        }
+        return null;
+    }
+}

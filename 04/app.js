c7ac2c2b071a5e939553df2c3e6591bf650fa445
Agnieszka Polińska <agapoli@gmail.com>
2021-03-03 15:27:03 +0100

    Implement and call function checkIfUserBirthdayIsToday()

--- 04/app.js
+++ 04/app.js
@@ -7,3 +7,19 @@ const user = {
         year: '1985'
     }
 }
+
+const checkIfUserBirthdayIsToday = (user) => {
+    const date = new Date();
+    const day = date.getDay() < 10 ? `0${date.getDay()}` : date.getDay();
+    const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+
+    if (user.born.day === day && user.born.month === month) {
+        return `It's ${user.firstName} ${user.lastName}'s birthday!`;
+    }
+    return `It's not ${user.firstName} ${user.lastName}'s birthday.`;
+}
+
+console.log(checkIfUserBirthdayIsToday(user));
+
+
+

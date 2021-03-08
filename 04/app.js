5367fdcb62a301812bde2ce75235033d53b1a0cd
Konrad Ciepluch <konrad190008@wp.pl>
2021-03-09 00:18:04 +0100

    Add function showBirthday

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,27 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '9',
+        month: '03',
         year: '1985'
     }
 }
+
+const time = new Date();
+
+const day = time.getDate().toString();
+let month = time.getMonth();
+// powiększam o 1 wartość, ponieważ metoda indeksuje miesiące od 0
+month+=1;
+
+month = month < 10 ? '0' + month  : month ;
+
+function showBirthday() {
+    if(user.born.day == day && user.born.month == month ) {
+        console.log('Wszystkiego najlepszego Adam :)');
+    } else {
+        console.log('Adam nie obchodzi dziś urodzin :(');  
+    }
+}
+
+showBirthday();

70f506961e2d5467ceae8418d871829365f7f082
Konrad Ciepluch <konrad190008@wp.pl>
2021-03-08 21:41:57 +0100

    Add object declaration JS

---
+++ 01/app.js
@@ -0,0 +1,13 @@
+const user = {
+firstName : 'Anna',
+lastName : 'Nowak',
+sex : 'female',
+age : 20 
+};
+
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.sex);
+console.log(user.age);
+
+

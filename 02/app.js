a80e54bd07e6e6fb366878e2a11b62e2fe65a13d
Konrad Ciepluch <konrad190008@wp.pl>
2021-03-08 22:23:28 +0100

    Remove comments from app.js

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,3 @@
-/* W pliku `app.js` znajdziesz zmienną `calendarJS`, która zawiera informacje o wydaniach kolejnych wersji standardu dla języka JavaScript.
-
-Twoim zadaniem będzie wyświetlić te dane w konsoli przy użyciu pętli `for .. in`. Treść jaka ma się pojawić to odpowiednio dla pierwszego wpisu to: `ES1 wydano w terminie 1997-06`.
-
-Jeśli wartość dla danej właściwości będzie równa `null` (tak jak w przypadku ES4) to tekst w konsoli ma się prezentaować w ten sposób: `ES4 nie zostało wydane`.
-
-Pamiętaj, że do wartości właściwości możesz się odwołać przy pomocy konstrukcji `const valueFromObjectProp = object[variableWithPropName]`. */
-
 const calendarJS = {
     'ES1': '1997-06',
     'ES2': '1998-06',

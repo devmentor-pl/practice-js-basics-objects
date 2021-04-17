2cc3cd5fbf9aa4f2239f03607fa5593850b30d45
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-04-17 19:41:23 +0200

    Task 04

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,31 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
+        day: '17',
         month: '04',
         year: '1985'
     }
 }
+
+
+
+user.isBirthday = function() {
+
+    const now = new Date();
+    const month = parseInt(now.getMonth() + 1);
+    const day= parseInt(now.getDate());
+
+    if(month == this.born["month"] && day == this.born["day"]) {
+
+        return console.log(this.firstName + " dzis są twoje urodziny!");
+
+    } else {
+
+        return console.log(this.firstName + " jeszcze musisz poczekać");
+        
+    };
+
+};
+
+console.log(user.isBirthday());
+

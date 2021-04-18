c3b9c99dcec71f7015d08e76dcdc4fba27f5a386
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-04-18 16:57:09 +0200

    Task 01: object user
    Task 01: object user (firstName, lastName, sex, age)

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+
+let user = new Object();
+
+user = {
+    firstName: 'Gieniek',
+    lastName: 'Ogórkiewicz',
+    sex: 'male',
+    age: 45
+}
+
+console.log(user.firstName, user.lastName, user.sex, user.age)

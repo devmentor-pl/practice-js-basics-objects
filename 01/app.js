2fc6f87bd4b51471237968b03fd74891f8e2807c
Michal Krawczyk <krawczykmichal1@gamil.com>
2021-07-07 12:10:37 +0100

    done

---
+++ 01/app.js
@@ -0,0 +1,8 @@
+const user = {
+    firstName: 'Miki',
+    lastName: 'Mouse',
+    age: 99,
+    sex: 'unknown',
+}
+
+console.log(user);
--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,16 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '07',
+        month: '07',
         year: '1985'
     }
 }
+
+const date = new Date();
+const currentDay = date.UTCDate(),
+const currentMonth = date.getMonth(),
+
+if((user.born.day) == currentDay && (user.born.month) == currentMonth) {
+    console.log('Masz dzisiaj urodziny!!')
+} else {console.log('Dziś nie pijesz')};

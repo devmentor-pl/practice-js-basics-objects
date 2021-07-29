b54a575c5aa5535b8b2f67c98181da0074474d3d
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-07-29 02:23:48 +0200

    closes task 4

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,19 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '28',
+        month: '07',
         year: '1985'
     }
+}
+
+const birthday = Object.values(user.born).slice(0, 2).map(el => parseInt(el));
+
+checkBirthday(birthday);
+
+function checkBirthday(arr) {
+    const today = new Date();
+    (today.getDate() - arr[0] === 0 && (today.getMonth() + 1) - arr[1] === 0) ?
+        console.log(`${user.firstName} ma dziś urodziny :)`) :
+        console.log(`${user.firstName} nie ma dziś urodzin :(`);
 }

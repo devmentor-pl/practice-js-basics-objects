cfb84dd7751aeb51d2ad2857f0ba41c259250e96
Jakub Chrobak <jchrobakk@gmail.com>
2021-08-09 15:54:20 +0200

    Finish task 04

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,26 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+  firstName: "Adam",
+  lastName: "Nowak",
+  born: {
+    day: "9",
+    month: "08",
+    year: "1985",
+  },
+};
+
+const userBirthday = new Date(
+  user.born.year,
+  user.born.month - 1,
+  user.born.day
+);
+
+const now = new Date();
+
+if (
+  now.getMonth() === userBirthday.getMonth() &&
+  now.getDate() === userBirthday.getDate()
+) {
+  console.log("Użytkownik obchodzi dziś urodziny");
+} else {
+  console.log("Użytkownik nie obchodzi dziś urodzin");
 }

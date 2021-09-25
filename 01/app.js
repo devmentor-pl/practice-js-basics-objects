9b19e14061cedd4e26bc5bb6183b142a6848ddc5
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-25 13:22:16 +0200

    First task

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+const user = {
+    firstName: 'Andrzej',
+    lastName: 'Kowalski',
+    sex: 'man',
+    age: '41'
+}
+
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.sex);
+console.log(user.age);

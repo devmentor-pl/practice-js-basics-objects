ce15e7057eee3748b05a45b3baece58c2a29c60b
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-26 14:57:04 +0200

    Last task

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,27 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
-        year: '1985'
+        day: '21',
+        month: '03',
+        year: '1984'
     }
 }
+
+user.getBirthdayDay = function() {
+
+    const today = new Date().toLocaleDateString();
+    const date = this['born'];
+    const baseDate =  date['day'] + "." + date['month'] + "." + date['year'];
+
+    if ( baseDate === today) {
+        console.log("Today is the birthday of " + this['firstName'] + ' ' + this['lastName'])
+    }
+    else {
+        console.log('Today is no birthdays');
+    }
+
+}
+
+user.getBirthdayDay();
+
+// Myślę, żę da się to zrobić innym sposobem, ale ten wydawał mi się najprostszy :)

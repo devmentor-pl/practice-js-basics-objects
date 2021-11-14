f5908a970ea996057db5f266fd4927096368986e
Ewelina <ewelina@ekopacz.pl>
2021-11-14 17:52:58 +0100

    add part of function

--- 04/app.js
+++ 04/app.js
@@ -3,7 +3,24 @@ const user = {
     lastName: 'Nowak',
     born: {
         day: '14',
-        month: '04',
+        month: '10',
         year: '1985'
     }
 }
+
+const now = new Date();
+const nowDate = now.getDate();
+const nowMonth = now.getMonth();
+console.log('Dzisiejsza data to: ' + nowDate + " " + nowMonth);
+
+
+user.checkData = function(day,month) {
+    if(this['born']['day'] === day && this['born']['month']=== month){
+        console.log('Adam Nowak ma dzis urodziny! Wszytkiego najlepszego Adam');
+    }
+    else {
+       console.log('Adam Nowak nie ma dziś urodzin!');
+    }
+}
+
+user.checkData(nowDate,nowMonth);

aae0b7e2ee1d44739262643c9bf25e13b197f6ad
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-03-04 18:17:43 +0100

    Finish 04

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,21 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '04',
+        month: '03',
         year: '1985'
     }
+}
+
+
+console.log(parseInt(user.born.day), Number(user.born.month));
+
+const currDate = new Date();
+const currDay = currDate.getDate();
+const currMonth = currDate.getMonth() +1;
+
+console.log(currDay, currMonth);
+
+if(parseInt(user.born.day) === currDay && Number(user.born.month) === currMonth) {
+    console.log(user.firstName + ' ' + user.lastName + ' ma dziś urodziny!');
 }

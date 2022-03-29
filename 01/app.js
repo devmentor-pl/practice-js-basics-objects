314dd15b9d398e8d78950d14813b04ebc209ee07
Andrzej <andis_as@wp.pl>
2022-03-29 19:58:03 +0200

    zad 1 - done

---
+++ 01/app.js
@@ -0,0 +1,14 @@
+
+const user = {
+    firstName: 'Antoni',
+    lastName: 'Kosiba',
+    sex: 'male',
+    age: '20',
+}
+
+// two different ways to read the value of an object
+console.log(user)
+console.log(user.firstName)
+console.log(user.lastName)
+console.log(user['sex'])
+console.log(user['age'])

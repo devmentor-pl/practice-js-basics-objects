cb1e855e26c612adbac71460ea378b6b26049ce1
Camila Montes Celinski <camicelinski@gmail.com>
2022-04-03 05:22:15 +0200

    Finish task 04

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,16 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
+        day: '03',
         month: '04',
         year: '1985'
     }
+}
+
+const currDate = new Date();
+const currDay = currDate.getDate();
+const currMonth = currDate.getMonth() + 1;
+
+if(currDay === parseInt(user.born.day) & currMonth === parseInt(user.born.month)) {
+    console.log('Gratulacje! ' + user.firstName + ' ' + user.lastName + ' ma dziś urodziny.')
 }

9604581ead26d3ecabae61bdc984802ee8884043
Patryk Kropop <patryk.kropop@gmail.com>
2022-04-04 18:45:09 +0200

    finished task

---
+++ 01/app.js
@@ -0,0 +1,13 @@
+const user = {
+    firstName: 'Jan',
+    lastName: 'Kowalski',
+    sex: 'Mężczyzna',
+    age: 30,
+}
+
+
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.sex);
+console.log(user.age);
+

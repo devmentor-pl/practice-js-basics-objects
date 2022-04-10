3199829d4b3890c9639074e59a6e7584272763d9
Beata <beata.ptak11@gmail.com>
2022-04-10 20:16:52 +0200

    Zadanie-obiekty

---
+++ 01/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var user = {
+firstName: 'Beata',
+lastName: 'Ptak',
+sex: 'woman',
+age: 24,
+}
+
+console.log(user.firstName)
+console.log(user.lastName)
+console.log(user.sex)
+console.log(user.age)

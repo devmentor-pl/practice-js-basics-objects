0a5ea90a903e4aab07ac6859e97e78417ff3cee9
Mariana Kruk <krukmaryana@gmail.com>
2022-04-26 01:10:16 +0200

    task 4

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,30 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
+        day: '26',
         month: '04',
         year: '1985'
     }
 }
+
+user.checkBirthday = function() {
+
+    const now = new Date();
+    const dayNow = now.getDate();
+    const monthNow = now.getMonth() + 1;
+
+
+
+    const dayUser = this['born']['day'];
+    const monthUser = this['born']['month'];
+
+
+    if (dayUser == dayNow && monthUser == monthNow) {
+        return true;
+    }
+    return false;
+
+}
+
+const isBirthdayToday = user.checkBirthday();
+console.log(isBirthdayToday);

d3ad76b1faadcabfc657a82c80678165074e08ff
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-05-02 17:57:46 +0200

    Done 04 task

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,29 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+	firstName: 'Adam',
+	lastName: 'Nowak',
+	born: {
+		day: '04',
+		month: '04',
+		year: '1985',
+	},
+};
+
+const fullDate = getCurrentDate();
+
+function getCurrentDate() {
+	const now = new Date();
+	return now;
 }
+
+function isBirthdatToday(now, obj) {
+	const currentlyMonth = now.getMonth() + 1;
+	const currentlyDay = now.getDate();
+
+	if (currentlyDay == obj.born['day'] && currentlyMonth == obj.born['month']) {
+		console.log('Dziś są Twoje urodziny!!!');
+	} else {
+		console.log('Niestety to nie są Twoje urodziny :(');
+	}
+}
+
+isBirthdatToday(fullDate, user);

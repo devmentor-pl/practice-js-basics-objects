b3410537f19fff078a7e206ff2eef15087a4cc4b
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-05-20 21:05:32 +0200

    end task 02

---
+++ 01/app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const user = {
+    firstName: 'Aga',
+    lastName: 'Dra',
+    sex: 'female',
+    age: 33
+}
+
+console.log(user['firstName'], user['lastName'], user['sex'], user['age'] )

9444821cd1d7540e25bc21413040e231075ad5d3
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-05-22 10:44:40 +0200

    end task 04

--- 04/app.js
+++ 04/app.js
@@ -3,7 +3,27 @@ const user = {
     lastName: 'Nowak',
     born: {
         day: '14',
-        month: '04',
+        month: '4',
         year: '1985'
     }
 }
+
+const bornDay = Number(user['born']['day'])
+const bornMonth = Number(user['born']['month'])
+const bornYear = Number(user['born']['year'])
+
+const now = new Date();
+
+const year = now.getFullYear()
+const month = now.getMonth()
+const day = now.getDate()
+
+const check = function(){
+   
+    if(bornDay&&bornMonth&&bornYear===day&&month&&year){
+    return 'yes'
+
+} else return 'no'
+}
+
+console.log(check())

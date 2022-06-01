916996b0c26f7d7761b7857805041ee87e07200c
Ada <adriannazalewska1@gmail.com>
2022-06-01 17:31:35 +0200

    done

---
+++ 01/app.js
@@ -0,0 +1,8 @@
+const user = {
+    firstName: "Ada",
+    lastName: "Zalewska",
+    sex: "female",
+    age: 25,
+}
+
+console.log(user);
--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,17 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '02',
+        month: '06',
         year: '1985'
     }
+}
+const currDate = new Date();
+const currDay = currDate.getDate();
+const currMonth = currDate.getMonth() +1;
+
+if (parseInt(user.born.day) === currDay && parseInt(user.born.month) === currMonth) {
+    console.log ('HAPPY B DAY')
+} else {
+    console.log('Today is not your BDay')
 }

01f10489709d37b54e06fc8cd6b75750f0580de5
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-06-19 18:49:44 +0200

    task 4 done

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,21 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+	firstName: "Adam",
+	lastName: "Nowak",
+	born: {
+		day: "14",
+		month: "04",
+		year: "1985",
+		// day: "19",
+		// month: "06",
+		// year: "1985",
+	},
+};
+const currentDate = new Date();
+const currentDay = currentDate.getDate();
+const currentMonth = currentDate.getMonth() + 1;
+
+if (parseInt(user.born.day) === currentDay && parseInt(user.born.month) === currentMonth) {
+	console.log("Dziś są Adama urodziny. Pamiętaj aby złożyć mu życzenia.");
+} else {
+	console.log("Dziś Adam nie obchodzi urodzin.");
 }

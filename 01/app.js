9a8dffbcce5cac0853c6f891ff89a1c6d8149317
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-06-29 12:59:38 +0200

    Finished tasks - objects

---
+++ 01/app.js
@@ -0,0 +1,7 @@
+const user = {
+	firstName: "Jan",
+	lastName: "Kowalski",
+	sex: "M",
+	age: 35,
+};
+console.log(user.firstName, user.lastName, user.sex, user.age);
--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,18 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
-}
+	firstName: "Adam",
+	lastName: "Nowak",
+	born: {
+		day: "27",
+		month: "04",
+		year: "1985",
+	},
+};
+
+const today = new Date();
+const currentDay = today.getDate();
+const currentMonth = today.getMonth() + 1;
+
+// Dla porównania oprócz wartości również typu danych możemy użyć "parseInt()" do sprowadzenia wartości w obiekcie 'user' ze string do typu "number" lub "toString()" do wartości typu "number" z new Date() do typu "string" , wówczas sprawdzam przy pomocy "==="
+user.born.day == currentDay && user.born.month == currentMonth
+	? console.log("Birthday")
+	: console.log("Not today");

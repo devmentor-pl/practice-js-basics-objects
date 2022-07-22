49b7de49c76c22873cfe5310fc8831500d7e7121
Zuza Ganobis <zuzaganobis@gmail.com>
2022-07-22 10:54:51 +0200

    tasks completed

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,22 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+  firstName: "Adam",
+  lastName: "Nowak",
+  born: {
+    day: "14",
+    month: "04",
+    year: "1985",
+  },
+};
+
+const now = new Date();
+
+let month = now.getMonth() + 1;
+let day = now.getDate();
+
+if (month < 10) {
+  month = "0" + month;
+}
+
+if (month == user.born.month && day == user.born.day) {
+  console.log("Sto lat!");
 }

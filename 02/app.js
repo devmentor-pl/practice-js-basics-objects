42743f85c0821421b0637d7d8c244b0a56855ce6
Jacek Zielinski <gandi34@wp.pl>
2022-09-10 15:41:25 +0200

    02 practice basics objects

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,15 @@
 const calendarJS = {
-    'ES1': '1997-06',
-    'ES2': '1998-06',
-    'ES3': '1999-12',
-    'ES4': null,
-    'ES5': '2009-12',
-    'ES6': '2015-06',
-    'ES7': '2016-06',
-    'ES8': '2017-06',
-    'ES9': '2018-06',
+    'ES1':'Es1 wydano w latach 1997-06',
+    'ES2':'Es2 wydano w latach 1998-06',
+    'ES3':'Es3 wydano w latach 1999-12',
+    'ES4': 'Es4 nie zostało wydane',
+    'ES5':'Es5 wydano w latach 2009-12',
+    'ES6':'Es6 wydano w latach2015-06',
+    'ES7':'Es7 wydano w latach2016-06',
+    'ES8':'Es8 wydano w latach2017-06',
+    'ES9':'Es9 wydano w latach2018-06',
+}
+
+for (const java in calendarJS) {
+    console.log(calendarJS[java]);
 }

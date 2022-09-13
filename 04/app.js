423e2e74557964dc3270654944d56ffbd3c9606c
Jacek Zielinski <gandi34@wp.pl>
2022-09-13 13:16:16 +0200

    04 done

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,29 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '13',
+        month: '09',
         year: '1985'
     }
+}
+
+const today = new Date();
+const day = today.getDate();
+console.log(day);
+
+const month = today.getMonth() +1;
+console.log(month);
+
+const userMonth = parseInt(user.born.month);
+console.log(userMonth);
+
+const userDay = parseInt(user.born.day);
+console.log(userDay);
+
+
+if (userMonth === month && userDay === day ) {
+    console.log('Gratulacje')
+}
+else {
+    console.log ('Przykro mi')
 }

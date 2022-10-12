f39d86ca845ae3cd6e87360df080c622668a46ab
Norbert sasak <norbert.sasak@gmail.com>
2022-10-12 19:42:29 +0200

    task done

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,39 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
-        year: '1985'
+        day: '13',
+        month: '10',
+        year: '2022'
+        // day: '12',
+        // month: '04',
+        // year: '1985'
     }
+}
+
+
+
+const birthday = new Date();
+const currentTime = new Object();
+currentTime.day = birthday.getDate();
+currentTime.month = birthday.getMonth() + 1;
+currentTime.year = birthday.getFullYear();
+
+const beta = [];
+for (const keys in currentTime) {
+    // console.log(currentTime[keys]);
+    beta.push(currentTime[keys])
+}
+
+const birthdayUser = Object.assign({}, user.born);
+
+const alfa = [];
+for (const key in birthdayUser) {
+    // console.log(birthdayUser[key])
+    alfa.push(birthdayUser[key]);
+}
+
+if (alfa[0] == beta[0] && alfa[1] == beta[1] && alfa[2] == beta[2]) {
+    console.log(`Użytkownik ma dzisiaj urodziny.`);
+} else {
+    console.log(`Użytkownik nie ma dzisiaj urodzin.`);
 }

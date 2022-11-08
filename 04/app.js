4e420822fe9a20a4c69760becc348947613c2f6c
Anna <aniamatrackasadurek@gmail.com>
2022-11-08 10:50:55 +0100

    exercise 4 1st part finished

--- 04/app.js
+++ 04/app.js
@@ -12,4 +12,12 @@ const day = new Date().getDate();
 const month = new Date().getMonth() + 1;
 const year = new Date().getFullYear();
 
-function isUserBirthday() {}
+function isUserBirthday() {
+
+    if((user.born.day === day) && (user.born.month === month)) {
+        return console.log('Happy Birthday!');
+    }
+    return console.log('Not Happy Birthday! but Have A Nice Day! ;) ');
+}
+
+isUserBirthday()

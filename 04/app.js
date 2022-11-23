a93772d6e774b10a394836d234f38eb5bbf78c8a
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-11-23 13:53:32 +0100

    Finish task 4

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,36 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
-}
+	firstName: 'Adam',
+	lastName: 'Nowak',
+	born: {
+		day: '14',
+		month: '04',
+		year: '1985',
+	},
+};
+const user2 = {
+	firstName: 'Adamadsa',
+	lastName: 'Nowakasdas',
+	born: {
+		day: '14',
+		month: '04',
+		year: '1994',
+	},
+};
+
+const date = new Date();
+const currentDay = date.getDate();
+const currentMonth = date.getMonth() + 1;
+
+user.checkBirthday = function () {
+	if (
+		parseInt(this.born.day) === currentDay &&
+		parseInt(this.born.month) === currentMonth
+	) {
+		return console.log(
+			`Wszystkiego najlepszego ${this.firstName} ${this.lastName}!`
+		);
+	}
+	return null;
+};
+
+user.checkBirthday();

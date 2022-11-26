e80ac1da1c131c3390f710149617943c69510b2f
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-11-27 00:14:48 +0100

    04 done

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,22 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '27',
+        month: '11',
         year: '1985'
     }
 }
+
+user.Birthday = function() {
+    const now = new Date();
+    const day = this['born']['day'];
+    const month = this['born']['month'];
+    const year = this['born']['year'];
+    const date = new Date(year, month ,day);
+    
+    console.log(now.getMonth())
+    if((now.getDate() == day) && (now.getMonth()+1 == month)) console.log("wszystikego najlepszego!");
+    else console.log("to jeszcze nie ten dzień")
+}
+
+user.Birthday();

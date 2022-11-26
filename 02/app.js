5e87765487605bf786b15438c520c4e1d427beef
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-11-26 13:47:36 +0100

    02 done

--- 02/app.js
+++ 02/app.js
@@ -9,3 +9,7 @@ const calendarJS = {
     'ES8': '2017-06',
     'ES9': '2018-06',
 }
+for(const key in calendarJS) {
+    if(calendarJS[key] == null) console.log(key, "nie zostało wydane");
+    else console.log(key, "zostało wydne w terminie", calendarJS[key]);
+}

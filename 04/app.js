3aa124fd1d92df509da7975b4517511f0200f8c8
Michal Filipowski <mikefilipowski@gmail.com>
2022-12-17 17:20:01 +0100

    practice-js-basics-objects done

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,21 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '10',
+        month: '03',
         year: '1985'
     }
 }
+
+
+function birthday(){
+    if(parseInt(user.born.day) === currDay && parseInt(user.born.month) === currMonth){
+        console.log('Wszystkiego najlepszego ! Dzis sa Twoje urodziny !')
+    }
+}
+
+const date = new Date();
+const currDay = date.getDate();
+const currMonth = date.getMonth();
+
+birthday()

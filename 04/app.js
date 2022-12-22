69f6acfd505121722282e2eae4f63d74a01a2e7d
Michal Lata <maxpoz3212@gmail.com>
2022-12-22 21:47:48 +0100

    Add check birthday program

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,25 @@
+function birthdayCheck() {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const dateToday = day + '/' + month;
+    const userBorn = Object.values(this.born).join('/');
+    const userBirthday = userBorn.slice(0, -5);
+
+    if (dateToday === userBirthday) {
+        console.log(`Today is ${this.firstName}'s birthday. Happy Birthday ${this.firstName}!`);
+    } else {
+        console.log(`Today isn't ${this.firstName}'s birthday :(. ${user.firstName}'s birthday is in ${userBirthday} – remember that date!`);
+    }
+}
+
 const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '22',
+        month: '12',
         year: '1985'
-    }
+    },
+    isBirthday: birthdayCheck,
 }

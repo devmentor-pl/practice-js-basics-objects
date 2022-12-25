bad40bb071099c415f79d95a0d86c230bef5aaab
Michal Lata <maxpoz3212@gmail.com>
2022-12-25 10:18:42 +0100

    Add fix in script

--- 04/app.js
+++ 04/app.js
@@ -1,10 +1,20 @@
 function checkBirthday() {
-    const date = new Date();
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const userBorn = this.born.day + '.' + this.born.month;
+    const date = new Date('2022-02-01');
+    let day = date.getDate().toString();
+    let month = (date.getMonth() + 1).toString();
+    const userBornDay = this.born.day.toString();
+    const userBornMonth = this.born.month.toString();
+    const userBorn = userBornDay + '.' + userBornMonth;
 
-    if (day.toString() === this.born.day.toString() && month.toString() === this.born.month.toString()) {
+    if (day.length === 1 && userBornDay.length === 2) {
+        day = '0' + day;
+    }
+
+    if (month.length === 1 && userBornMonth.length === 2) {
+        month = '0' + month;
+    }
+
+    if (day === userBornDay && month === userBornMonth) {
         return console.log(`Today is ${this.firstName}'s birthday. Happy Birthday ${this.firstName}!`);
     } else {
         return console.log(`Today is not ${this.firstName}'s birthday :(. ${this.firstName}'s birthday is in ${userBorn} – remember that date!`);
@@ -15,8 +25,8 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '23',
-        month: '12',
+        day: '01',
+        month: '02',
         year: '1985'
     },
     isBirthday: checkBirthday,

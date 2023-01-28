52a738250725136734d8df13197804018ff26e3e
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-01-28 10:17:42 +0100

    correct task-04

--- 04/app.js
+++ 04/app.js
@@ -2,18 +2,18 @@ const user = {
 	firstName: 'Adam',
 	lastName: 'Nowak',
 	born: {
-		day: '27',
-		month: '00',
+		day: '28',
+		month: '01',
 		year: '1985',
 	},
 }
 
 const date = new Date()
-const userBirthday = new Date(0, user.born.month, user.born.day)
+const userBirthday = new Date(0, user.born.month - 1, user.born.day)
 const birthdayDay = new Date(0, date.getMonth(), date.getDate())
 
 if (userBirthday.getTime() === birthdayDay.getTime()) {
 	console.log('Użytkownik ma dziś urodziny!')
 } else {
-	console.log('Nie ma dziś urodzin')
+	console.log('Użytkownik nie ma dziś urodzin.')
 }

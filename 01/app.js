f62a26c021d0a8a27e1aa761dd8f0ffffae31621
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-02-03 19:22:51 +0100

    rozwiązanie zadań obiekty

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+const user = {
+  firstName: "Anna",
+  lastName: "Giewont",
+  sex: "Female",
+  age: "35",
+};
+
+console.log(user.firstName);
+console.log(user["lastName"]);
+console.log(user.sex);
+console.log(user["age"]);
--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,28 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+  firstName: "Adam",
+  lastName: "Nowak",
+  born: {
+    day: "08",
+    month: "04",
+    year: "1985",
+  },
+};
+
+const now = new Date();
+const currentDay = now.getDate();
+const currentMonth = now.getMonth();
+
+const day = Number(user.born.day);
+const month = Number(user.born.month);
+
+birthdayCheck();
+
+function birthdayCheck() {
+  if (day === currentDay && month === currentMonth) {
+    console.log(user.firstName + " ma dziś urodziny!");
+    return true;
+  } else {
+    console.log(user.firstName + " nie ma dziś urodzin");
+    return false;
+  }
 }

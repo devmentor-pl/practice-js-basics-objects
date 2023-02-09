f29da94f82ee252e89663226834dafccb91801c3
anetasobanska <sannettka@gmail.com>
2023-02-09 15:46:15 +0100

    Finish #04

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,17 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '09',
+        month: '02',
         year: '1985'
     }
+}
+
+const date = new Date();
+const day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+const month = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+console.log(day, month);
+
+if(user.born.day === day && user.born.month === month) {
+    console.log("Masz dzis urodziny!");
 }

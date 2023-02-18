74fce40df0d5539bf05b9603a2949a30107c8f06
Wioleta Lipinska <hello@wiolipcreates.pl>
2023-02-18 18:43:38 +0100

    task 02

--- 01/app.js
+++ 01/app.js
@@ -10,3 +10,6 @@ console.log(user.firstName);
 console.log(user.lastName);
 console.log(user.sex);
 console.log(user.age);
+
+// można w jedne linii i z użyciem [ ]
+console.log(user.firstName, user.lastName, user['sex'], user['age'])

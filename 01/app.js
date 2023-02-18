1e6ec880c3eccbf92684ea0ad0ec98a10be456b6
Wioleta Lipinska <hello@wiolipcreates.pl>
2023-02-18 17:47:12 +0100

    taks1

---
+++ 01/app.js
@@ -0,0 +1,12 @@
+const user = {
+    firstName : "Wioleta",
+    lastName : "Lip",
+    sex: "women",
+    age: 28
+
+}
+
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.sex);
+console.log(user.age);

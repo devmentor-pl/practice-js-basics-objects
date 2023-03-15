2802852906bb15e5343d4c846c16a42f48fe4f76
Piotr Mazurek <mazurekp93@gmail.com>
2023-03-15 16:29:57 +0100

    Finish task-04

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,21 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '15',
+        month: '03',
         year: '1985'
     }
 }
+
+user.checkBirthday = function (day, month, year) {
+    const date = (new Date).toLocaleDateString().split('.')
+    const actualDay = date[0]
+    const actualMonth = date[1]
+    const actualYear = date[2]
+
+    if (day === actualDay && month === actualMonth) {
+        return `${this.firstName} obchodzi dziś ${actualYear - year} urodziny`
+    }
+    return `${this.firstName} nie ma dziś urodzin`
+}
+console.log(user.checkBirthday(user.born.day, user.born.month, user.born.year));

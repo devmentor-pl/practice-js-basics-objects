203f6760ae7178dfef2c95735c1173478e937db3
Ireneusz Cislo <irekc86@o2.pl>
2023-03-24 22:26:42 +0100

    task 02 finished

--- 02/app.js
+++ 02/app.js
@@ -9,3 +9,11 @@ const calendarJS = {
     'ES8': '2017-06',
     'ES9': '2018-06',
 }
+
+for(key in calendarJS) {
+    if(calendarJS[key]) {
+        console.log(key + ' wydano w terminie ' + calendarJS[key])
+    } else {
+        console.log(key + ' nie zostało wydane ')
+    }
+}

f8698314a8722526a03bb69dcfba5a94ef5997e5
Ireneusz Cislo <irekc86@o2.pl>
2023-03-24 22:40:08 +0100

    task 02 finished

--- 03/app.js
+++ 03/app.js
@@ -42,10 +42,30 @@ books.getAuthor = function(isbn) {
 }
 
 books.getTitle = function(isbn, lang) {
+    if(typeof this[isbn] === 'undefined') {
+        return null
+    }
+
+    const title = this[isbn]['title'][lang];
+    if(title) {
+        return title
+    }
+
+    return false;
 
 }
 
 books.getTranslator = function(isbn, lang) {
+    if(typeof this[isbn] === 'undefined') {
+        return null
+    }
+
+    const translator = this[isbn]['translator'][lang];
+    if(translator) {
+        return translator
+    }
+
+    return false;
 
 }
 

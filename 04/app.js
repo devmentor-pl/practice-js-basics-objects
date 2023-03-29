91134c1891b96f31a1834aae2bb0cc81c1917527
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-03-29 14:56:22 +0200

    finished task

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,28 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+  firstName: "Adam",
+  lastName: "Nowak",
+  born: {
+    day: "29",
+    month: "03",
+    year: "1985",
+  },
+};
+
+const now = new Date();
+const nowYear = now.getFullYear();
+const nowMonth = convertDate(now.getMonth() + 1);
+const nowDay = convertDate(now.getDate());
+
+const nowDate = `${nowMonth}-${nowDay}`;
+const userDate = `${user.born.month}-${user.born.day}`;
+
+if (nowDate === userDate) {
+  console.log(`Masz dzisiaj ${nowYear - Number(user.born.year)} urodziny`);
+}
+
+function convertDate(date) {
+  if (date < 10) {
+    return "0" + date;
+  }
+  return date;
 }

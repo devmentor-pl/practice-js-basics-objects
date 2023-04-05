b5226fd4ffa416e93ac9afe3e77633b663eb094a
Maria Kardas <mariah.grze@gmail.com>
2023-04-05 19:57:29 +0200

    Finished task 01

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+let user = {
+    firstName: 'Maria',
+    lastName: 'Mariowa',
+    sex: 'female',
+    age: 30,
+}
+
+console.log(user.firstName)
+console.log(user.lastName)
+console.log(user.sex)
+console.log(user.age)

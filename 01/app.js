d632a36fa18cfa3fabb7d51faa7d6dfc1406f34a
kozerka <kozerka@gmail.com>
2023-04-07 21:13:12 +0200

    Complete task 01

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+const user = {
+  firstName: "Ola",
+  lastName: "Kowalska",
+  sex: "female",
+  age: 25,
+};
+
+console.log(user);
+console.log(
+  `firstName:${user.firstName}, lastName:${user.lastName}, sex:${user.sex}, age:${user.age}`
+);

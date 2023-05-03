d4c588549f2a20729b05eb3770d0111c06105e5f
mike161pl <mike161pl@gmail.com>
2023-05-03 13:07:44 +0200

    task finished

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,22 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+	firstName: 'Adam',
+	lastName: 'Nowak',
+	born: {
+		day: '3',
+		month: '5',
+		year: '1985',
+	},
 }
+const birthdayShow = function () {
+	const dateToday = new Date()
+	const dayToday = dateToday.getDate()
+	const monthToday = dateToday.getMonth() + 1
+    
+	if (dayToday == 3 && monthToday == 5 ) {
+		console.log(`${user.firstName} masz dziś urodziny`)
+	} else {
+		console.log(`${user.firstName} dziś nie masz urodzin`)
+	}
+}
+
+birthdayShow(user.born.day, user.born.month)

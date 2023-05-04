7192c18dcacbda8d05e67af51f35489bf4569974
mike161pl <mike161pl@gmail.com>
2023-05-04 14:41:16 +0200

    refactoring

--- 04/app.js
+++ 04/app.js
@@ -2,21 +2,30 @@ const user = {
 	firstName: 'Adam',
 	lastName: 'Nowak',
 	born: {
-		day: '3',
-		month: '5',
+		day: '4',
+		month: '05',
 		year: '1985',
 	},
 }
 const birthdayShow = function () {
 	const dateToday = new Date()
-	const dayToday = dateToday.getDate()
-	const monthToday = dateToday.getMonth() + 1
-    
-	if (dayToday == 3 && monthToday == 5 ) {
+	const dayToday = startWithZero(dateToday.getDate())
+	const monthToday = startWithZero(dateToday.getMonth() + 1)
+	const birthDay = startWithZero(Number(user.born.day)) 
+	const birthMonth = startWithZero(Number(user.born.month))
+	console.log(typeof birthDay)
+    console.log(birthDay);
+	console.log(dayToday)
+	console.log(typeof dayToday)
+
+	if (dayToday === birthDay && monthToday === birthMonth) {
 		console.log(`${user.firstName} masz dziś urodziny`)
-	} else {
+	}
+   else {
 		console.log(`${user.firstName} dziś nie masz urodzin`)
 	}
 }
-
+function startWithZero(num) {
+	return (num < 10 ? '0' : '') + num
+}
 birthdayShow(user.born.day, user.born.month)

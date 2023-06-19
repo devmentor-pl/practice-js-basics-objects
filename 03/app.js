4d91ff389625ef2a0b00f090df09e3a33a311d2e
Wojciech Flasinski <wojciech.flasinski@gmail.com>
2023-06-19 21:44:47 +0200

    js-basic-object finish

--- 03/app.js
+++ 03/app.js
@@ -1,40 +1,40 @@
 const books = {
-    '978-83-7278-000-3': {
-        author: 'J.K. Rowling',
-        title: {
-            en: 'Harry Potter and the Philosopher\'s Stone',
-            pl: 'Harry Potter i Kamień Filozoficzny',
-        },
-        translator: {
-            en: null,
-            pl: 'Andrzej Polkowski',
-        }
+  '978-83-7278-000-3': {
+    author: 'J.K. Rowling',
+    title: {
+      en: 'Harry Potter and the Philosopher\'s Stone',
+      pl: 'Harry Potter i Kamień Filozoficzny',
     },
-    '83-7278-007-2': {
-        author: 'J.K. Rowling',
-        title: {
-            en: 'Harry Potter and the Chamber of Secrets',
-            pl: 'Harry Potter i Komnata Tajemnic ',
-        },
-        translator: {
-            en: null,
-            pl: 'Andrzej Polkowski',
-        }
+    translator: {
+      en: null,
+      pl: 'Andrzej Polkowski',
+    }
+  },
+  '83-7278-007-2': {
+    author: 'J.K. Rowling',
+    title: {
+      en: 'Harry Potter and the Chamber of Secrets',
+      pl: 'Harry Potter i Komnata Tajemnic ',
     },
+    translator: {
+      en: null,
+      pl: 'Andrzej Polkowski',
+    }
+  },
 }
 
 books.getAuthor = function(isbn) {
-    // w tym przypadku this === books
+  // w tym przypadku this === books
     
-    if(typeof this[isbn] === 'undefined') {
-        // taki ISBN nie istnieje w moim spisie
-        return null;
-    }
+  if(typeof this[isbn] === 'undefined') {
+    // taki ISBN nie istnieje w moim spisie
+    return null;
+  }
 
-    const author = this[isbn]['author'];
+  const author = this[isbn]['author'];
     if(author) {
-        // zwracam informacje o autorze
-        return author;
+    // zwracam informacje o autorze
+    return author;
     }
 
     // brak informacji o autorze
@@ -42,12 +42,39 @@ books.getAuthor = function(isbn) {
 }
 
 books.getTitle = function(isbn, lang) {
+        
+  if(typeof this[isbn] === 'undefined') {
+    return null;
+  } 
 
+  const title = this[isbn]['title'];
+    if (typeof title[lang] === 'undefined') {
+      return null;
+    } else {
+      if(lang === 'pl') {
+        return title['pl'];
+      } else if (lang === 'en') {
+        return title['en'];
+      } else return false;
+    }
 }
-
+  
 books.getTranslator = function(isbn, lang) {
+  if(typeof this[isbn] === 'undefined') {
+    return null;
+  } 
+
+  const translator = this[isbn]['translator'];
+  if (typeof translator[lang] === 'undefined') {
+    return null;
+  } else if (lang === 'pl') {
+    return translator['pl'];
+  } else if (lang === 'en' && translator[lang] === null) {
+    return false;
+    }
 
 }
+
 
 
 console.log( books.getAuthor('978-83-7278-000-3') ); // J.K. Rowling
@@ -56,3 +83,4 @@ console.log( books.getTitle('978-83-7278-000-3', 'pl') ); // Harry Potter i Kami
 console.log( books.getTitle('978-83-7278-000-3', 'en') ); // Harry Potter and the Philosopher's Stone
 console.log( books.getTranslator('83-7278-007-2', 'pl') ); // Andrzej Polkowski
 console.log( books.getTranslator('83-7278-007-2', 'en') ); // false
+
--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,26 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+	firstName: "Adam",
+	lastName: "Nowak",
+	born: {
+		day: "14",
+		month: "04", // było 04
+		year: "1985",
+	},
+};
+
+function birthday(person) {
+	// const now = new Date();
+	const now = new Date(2023, 4, 14);
+
+	const day = now.getDate();
+	const month = now.getMonth();
+
+  const date = person.born;
+
+  if (month == date['month'] && day == date['day']) {
+    return console.log('Happy Birthday !!!');
+  }
+  else return console.log('Never mind');
 }
+
+birthday(user)

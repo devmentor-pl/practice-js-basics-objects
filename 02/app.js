683223120f8510e6a4f07ecbcc4fbff6a3c2659e
Marcin Soltysik <profittax@gmail.com>
2023-07-10 23:27:23 +0200

    Done 02, objects

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,23 @@
 const calendarJS = {
-    'ES1': '1997-06',
-    'ES2': '1998-06',
-    'ES3': '1999-12',
-    'ES4': null,
-    'ES5': '2009-12',
-    'ES6': '2015-06',
-    'ES7': '2016-06',
-    'ES8': '2017-06',
-    'ES9': '2018-06',
+  ES1: '1997-06',
+  ES2: '1998-06',
+  ES3: '1999-12',
+  ES4: null,
+  ES5: '2009-12',
+  ES6: '2015-06',
+  ES7: '2016-06',
+  ES8: '2017-06',
+  ES9: '2018-06',
+};
+
+for (const key in calendarJS) {
+  if (Object.hasOwnProperty.call(calendarJS, key)) {
+    const element = calendarJS[key];
+
+    if (element === null) {
+      console.log(key + ' nie zostało wydane');
+    } else {
+      console.log(key + ' wydano w terminie ' + element);
+    }
+  }
 }

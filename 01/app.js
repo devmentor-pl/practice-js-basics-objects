ff5e8b1441e6e0a92faf68b3f90c70137e0e1c64
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-07-20 23:06:53 +0200

    tasks finished

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+const user = {
+    firstName: "Jan",
+    lastName: "Kowalski",
+    sex: "male",
+    age: 33,
+  };
+  
+  console.log(
+     `name:${user.firstName}, lastname: ${user.lastName}, sex: ${user.sex}, age: ${user.age}`
+  );
+  
--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,32 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+  firstName: "Adam",
+  lastName: "Nowak",
+  born: {
+    day: "14",
+    month: "04",
+    year: "1985",
+  },
+};
+
+function getDate() {
+  const date = new Date();
+  const dateToLocal = date.toLocaleDateString();
+
+  const splitDate = dateToLocal.split(".");
+  const userBirth = user.born;
+
+  const arrSplit = [];
+  const arrUser = [];
+
+  for (const key in splitDate) {
+    arrSplit.push(splitDate[key]);
+  }
+  for (const key in userBirth) {
+    arrUser.push(userBirth[key]);
+  }
+
+  let checkIfMatch = arrUser.every((i) => arrSplit.includes(i));
+  console.log(checkIfMatch);
 }
+
+getDate();

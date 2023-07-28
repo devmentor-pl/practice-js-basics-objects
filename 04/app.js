f1abe95349d6aa86bed3a80252dcf192820a59aa
Marcin <xcinek1337@wp.pl>
2023-07-28 21:30:49 +0200

    04

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,27 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+	firstName: 'Adam',
+	lastName: 'Nowak',
+	born: {
+		day: '28',
+		month: '07',
+		year: '1985',
+	},
+};
+
+function brithdayChecker(name) {
+	const userDay = this.born.day;
+	const userMonth = this.born.month;
+
+	const dateDay = new Date().getDate();
+	const dateMonth = new Date().getMonth() + 1;
+
+	if (dateMonth === 12) {
+		dateMonth = 0;
+	}
+
+	if (userDay == dateDay && userMonth == dateMonth) {
+		return 'the user has a birthday today';
+	} 
 }
+
+console.log(brithdayChecker.call(user));

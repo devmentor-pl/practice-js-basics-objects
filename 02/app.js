ae6222f146126fd061326d8dd171661e04ccc885
Marcin <xcinek1337@wp.pl>
2023-07-28 17:24:28 +0200

    02

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,18 @@
 const calendarJS = {
-    'ES1': '1997-06',
-    'ES2': '1998-06',
-    'ES3': '1999-12',
-    'ES4': null,
-    'ES5': '2009-12',
-    'ES6': '2015-06',
-    'ES7': '2016-06',
-    'ES8': '2017-06',
-    'ES9': '2018-06',
+	ES1: '1997-06',
+	ES2: '1998-06',
+	ES3: '1999-12',
+	ES4: null,
+	ES5: '2009-12',
+	ES6: '2015-06',
+	ES7: '2016-06',
+	ES8: '2017-06',
+	ES9: '2018-06',
+};
+
+for (const version in calendarJS) {
+	if (calendarJS[version] === null) {
+		calendarJS[version] = 'nie zostało wydane';
+	}
+	console.log(`${version} wydano w terminie ${calendarJS[version]}`);
 }

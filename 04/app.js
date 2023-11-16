b9697e0afd09b92f5a69482cd5da8f2eb90aeb81
Krzysztof Swół <krzysztof.swol@salesmanago.com>
2023-11-16 21:39:48 +0100

    feat: js-objects done

--- 04/app.js
+++ 04/app.js
@@ -7,3 +7,22 @@ const user = {
         year: '1985'
     }
 }
+
+const currDate = (new Date).toLocaleDateString();
+
+user.checkBirthday = function(date) {
+
+    if (typeof this.born === 'undefined') {
+        return 'Nie podano danych urodzenia';
+    }
+
+    const userBornDate = this.born.day + '.' + this.born.month + '.' + this.born.year;
+
+    if (userBornDate === date) {
+        return `${this.firstName} ma dzisiaj urodziny!`
+    }
+
+    return false;
+}
+
+console.log(user.checkBirthday(currDate));

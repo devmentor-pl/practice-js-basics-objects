ab41e8d057adb874403f15687b9cb513844ca0cb
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-21 01:43:12 +0100

    Check if user has a birthday today

--- 04/app.js
+++ 04/app.js
@@ -7,3 +7,15 @@ const user = {
         year: '1985'
     }
 }
+
+const currentDate = new Date(1985, 4, 14);
+
+if (
+    currentDate.getDate() === Number(user.born.day) &&
+    currentDate.getMonth() === Number(user.born.month) &&
+    currentDate.getFullYear() === Number(user.born.year)
+) {
+    console.log(`${user.firstName} ${user.lastName} ma dzisiaj urodziny!`)
+} else {
+    console.log(`${user.firstName} ${user.lastName} nie obchodzi dziś urodzin`)
+}

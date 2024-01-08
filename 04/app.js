263bf0861dbe2535a29f435f06923ec4b8eb1a57
Typee8 <wojciechjasiak1324@gmail.com>
2024-01-08 15:42:17 +0100

    Finish 04

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,36 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
-        year: '1985'
+        day: '08',
+        month: '01',
+        year: '1969'
     }
+}
+
+const now = new Date();
+
+const date = [now.getDate(), now.getMonth()]
+  .map(function (value) {
+    if (value === 0) {
+      value++;
+    }
+    if (value < 10) {
+      return "0" + value;
+    }
+  });
+
+const userBirthday = Object.values(user.born);
+
+todaysBirthday (date, userBirthday);
+
+function todaysBirthday (currentDate, userBirthday) {
+
+  for (let i = 0; i < currentDate.length; i++) {
+    if (currentDate[i] !== userBirthday[i]) {
+      return console.log("Today are no birthdays.");
+    }
+  }
+
+  return console.log("Today are " + user.firstName + " " + user.lastName + "'s birthday!");
+
 }

38bd89a550f3379005a4277cce9a122ed3edb1ff
Paweł Suder <pawel.suder94@gmail.com>
2024-02-09 12:01:49 +0100

    task 4 done

--- 04/app.js
+++ 04/app.js
@@ -7,3 +7,14 @@ const user = {
         year: '1985'
     }
 }
+
+const currentDate = new Date()
+const currentDay = currentDate.getDate()
+const currentMonth = currentDate.getMonth() + 1
+
+if (Number(user.born.day) === currentDay && Number(user.born.month) === currentMonth) {
+    console.log('Masz dziś urodziny!' + user.firstName + ' ' + user.lastName)
+} else {
+    console.log('Dziś nie obchodzisz swoich urodzin! ' + user.firstName + ' ' + user.lastName)
+}
+

c0bcee2b4504f56c00474dbd512bb77d2beb6558
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-03-06 21:40:10 +0100

    add tasks

--- 03/app.js
+++ 03/app.js
@@ -23,16 +23,16 @@ const books = {
     },
 }
 
-books.getAuthor = function(isbn) {
+books.getAuthor = function (isbn) {
     // w tym przypadku this === books
-    
-    if(typeof this[isbn] === 'undefined') {
+
+    if (typeof this[isbn] === 'undefined') {
         // taki ISBN nie istnieje w moim spisie
         return null;
     }
 
     const author = this[isbn]['author'];
-    if(author) {
+    if (author) {
         // zwracam informacje o autorze
         return author;
     }
@@ -41,18 +41,49 @@ books.getAuthor = function(isbn) {
     return false;
 }
 
-books.getTitle = function(isbn, lang) {
+books.getTitle = function (isbn, lang) {
+    if (typeof this[isbn] === 'undefined') {
+        return null;
+    }
 
+    const titleEng = this[isbn]['title']['en'];
+    const titlePl = this[isbn]['title']['pl'];
+    if (lang === 'en') {
+        return titleEng;
+    }
+    else if (lang === 'pl') {
+        return titlePl;
+    }
+    return false;
 }
 
-books.getTranslator = function(isbn, lang) {
+books.getTranslator = function (isbn, lang) {
+    if (typeof this[isbn] === 'undefined') {
+        return null;
+    }
 
+    const translatorEng = this[isbn]['translator']['en'];
+    const translatorPl = this[isbn]['translator']['pl'];
+
+    if (lang === 'en' && this[isbn]['translator']['en'] !== null) {
+        return translatorEng;
+
+    }
+
+    else if (lang === 'pl' && this[isbn]['translator']['pl'] !== null) {
+
+        return translatorPl;
+
+    }
+    return false;
 }
 
+console.log(books.getAuthor('978-83-7278-000-3')); // J.K. Rowling
+console.log(books.getAuthor('000-00-0000-000-0')); // null
+console.log(books.getTitle('978-83-7278-000-3', 'pl')); // Harry Potter i Kamień Filozoficzny
+console.log(books.getTitle('978-83-7278-000-3', 'en')); // Harry Potter and the Philosopher's Stone
+console.log(books.getTranslator('83-7278-007-2', 'pl')); // Andrzej Polkowski
+console.log(books.getTranslator('83-7278-007-2', 'en')); // false
 
-console.log( books.getAuthor('978-83-7278-000-3') ); // J.K. Rowling
-console.log( books.getAuthor('000-00-0000-000-0') ); // null
-console.log( books.getTitle('978-83-7278-000-3', 'pl') ); // Harry Potter i Kamień Filozoficzny
-console.log( books.getTitle('978-83-7278-000-3', 'en') ); // Harry Potter and the Philosopher's Stone
-console.log( books.getTranslator('83-7278-007-2', 'pl') ); // Andrzej Polkowski
-console.log( books.getTranslator('83-7278-007-2', 'en') ); // false
+
+

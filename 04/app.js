4ab3646530ff36a0240cf3e0e71f9bb7a7a45c2b
Jarek Stelmach <j.stelmach@onet.eu>
2024-03-10 18:24:55 +0100

    Making some progress in ex 4 in objects

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,51 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+	firstName: "Adam",
+	lastName: "Nowak",
+	born: {
+		day: "10",
+		month: "03",
+		year: "2024",
+	},
 }
+
+const userFirstName = user.firstName
+const userLastName = user.lastName
+const userDayBirth = user.born.day
+const userMonthBirth = user.born.month
+const userYearBirth = user.born.year
+
+const checkBirthDay = () => {
+	const fullDate = new Date()
+	let currentDay = fullDate.getDate()
+	let currentMonth = fullDate.getMonth()
+	const currentYear = fullDate.getFullYear()
+
+	console.log(typeof currentMonth)
+
+	if (currentDay < 10) {
+		currentDay = "0" + currentDay
+	} else {
+		currentDay
+	}
+
+	if (currentMonth < 10) {
+		currentMonth = "0" + currentMonth
+		currentMonth++
+	} else {
+		currentMonth++
+	}
+
+	if (
+		currentDay === userDayBirth &&
+		currentMonth === userMonthBirth &&
+		currentYear === userYearBirth
+	) {
+		console.log(
+			`Użytkownik ${userFirstName} ${userLastName} obchodzi dzisiaj urodziny.`
+		)
+	} else {
+		console.log("Dzisiaj nikt nie obchodzi urodzin.")
+	}
+}
+
+checkBirthDay()

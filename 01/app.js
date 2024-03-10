74186ac65ea80900a30529b3ae944e83fa9107ca
Jarek Stelmach <j.stelmach@onet.eu>
2024-03-10 12:24:10 +0100

    Finished ex 1 in objects

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+const user = {
+	firstName: 'Jan',
+	lastName: 'Kowalski',
+	sex: 'male',
+    age: 35,
+}
+
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.sex);
+console.log(user.age);

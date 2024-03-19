a7104cec58e144f3a5fa47d476cccd90a29d0314
MarcinMorgan <morgangzit@gmail.com>
2024-03-19 23:26:59 +0100

    feat: t1 display user data

---
+++ 01/app.js
@@ -0,0 +1,12 @@
+const user = {
+    firstName: 'Jan',
+    lastName: 'Nowak',
+    sex: 'm',
+    age: 54 
+}
+
+console.log('Dane użytkowniak:');
+console.log('Imię: '+user.firstName);
+console.log('Nazwisko: '+user.lastName);
+console.log('Płeć: '+user.sex);
+console.log('Wiek: '+user.age);

5b819a60b41d78081cb4715887a790fcc0c3e75b
Thanh Pham <126971526+healxer@users.noreply.github.com>
2024-04-02 22:18:07 +0200

    Finish task 4

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,21 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+  firstName: 'Adam',
+  lastName: 'Nowak',
+  born: {
+    day: '14',
+    month: '04',
+    year: '1985',
+  },
+};
+
+const currentDay = new Date().getDate();
+const currentMonth = new Date().getMonth() + 1;
+
+if (
+  currentDay === parseInt(user.born.day) &&
+  currentMonth === parseInt(user.born.month)
+) {
+  console.log('Happy Birthday dear ${user.firstName}!');
+} else {
+  console.log(`It's not your birthday today.`);
 }

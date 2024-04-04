d15d576b0cfe48006baaebffb216793143e597a7
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-04-04 14:41:30 +0200

    Task 04 finished

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,25 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
+        day: '05',
         month: '04',
         year: '1985'
     }
+}
+
+function checkBirthday(person) {
+    let today = new Date();
+    let currentDay = addLeadingZero(today.getDate());
+    let currentMonth = addLeadingZero(today.getMonth() + 1);
+    let birthdayDay = person['born']['day'];
+    let birthdayMonth = person['born']['month'];
+    console.log(`${currentDay} ${currentMonth} ${birthdayDay} ${birthdayMonth}`);
+    return (birthdayDay == currentDay && birthdayMonth == currentMonth);
+}
+
+
+console.log(checkBirthday(user));
+
+function addLeadingZero(input) {
+    return (input < 10 ? '0' : '') + input;
 }

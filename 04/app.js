15843d7bcc9a700e998f06986febff42998da1f1
BartekB94 <wurir1994@gmail.com>
2024-05-09 08:57:00 +0200

    complete 04

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,27 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
-        year: '1985'
+        day: '09',
+        month: '05',
+        year: '2024'
     }
+}
+
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() +1;
+const year = date.getFullYear();
+
+
+function addZeroToDate(num) {
+    return (num < 10 ? '0' : '') + num;
+}
+ 
+const userBirthday = new Date(user.born.year, user.born.month, user.born.day);
+
+
+if(year === userBirthday.getFullYear() & month === userBirthday.getMonth() & day === userBirthday.getDate()) {
+    console.log("Wszystkiego Nalpeszego!")
+} else {
+    console.log("Nie masz dzis urodzin")
 }

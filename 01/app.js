3e5708663c4cd491f16fdf4a867909bff2d23796
zero_jeden <zeroitjeden@gmail.com>
2024-06-09 20:11:02 +0200

    clean & save

--- 01/app.js
+++ 01/app.js
@@ -1,11 +1,11 @@
 const user = {
-    firstName: 'zero',
-    lastName: 'jeden',
-    sex: 'male',
-    age: 69,
-}
+	firstName: 'zero',
+	lastName: 'jeden',
+	sex: 'male',
+	age: 69,
+};
 
 console.log(user['firstName']);
 console.log(user['lastName']);
-console.log(user['sex'] );
+console.log(user['sex']);
 console.log(user['age']);

854cc6ab43e94fff5d77b5db7b821f0d8d06499d
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-22 00:29:28 +0200

    task 4 completed

--- 04/app.js
+++ 04/app.js
@@ -7,3 +7,16 @@ const user = {
         year: '1985'
     }
 }
+
+const currentDate = new Date()
+
+const userDate = user.born.day + user.born.month 
+const currentDateFormated = startWithZero(currentDate.getDate()) + "" + startWithZero((currentDate.getMonth() + 1))
+
+if(userDate == currentDateFormated) {
+    console.log("Masz Dzisiaj Urodziny!!")
+} else console.log("Nie masz dzisiaj urodzin")
+
+function startWithZero(number) {
+    return (number < 10 ? '0' : '') + number
+}

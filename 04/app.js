4805cda34f3d8c2c29c6e07ce94478b42dc909f7
marrcelp <marcel.piaszczyk@gmail.com>
2024-11-28 21:17:22 +0100

    resolved ex 4

--- 04/app.js
+++ 04/app.js
@@ -7,3 +7,25 @@ const user = {
         year: '1985'
     }
 }
+
+const currentDate = new Date();
+const currentDay = currentDate.getDate();
+const currentMonth = currentDate.getMonth() + 1;
+
+
+function checkBirthday(user){
+
+    const userBirthday = user.born;
+    const birthdayDay = parseInt(userBirthday.day);
+    const birthdayMonth = parseInt(userBirthday.month);
+
+    if (birthdayDay == currentDay && birthdayMonth == currentMonth) {
+        return 'Wszystkiego najlepszego z okazji dzisiejszych urodzin!';
+    }
+    else {
+        return 'Dzis nie masz urodzin'
+    }
+
+}
+
+console.log(checkBirthday(user));

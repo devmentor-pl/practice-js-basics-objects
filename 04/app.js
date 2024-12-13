3d374eb43c9e966bd256cae782569a8f4514fe8b
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-13 22:26:24 +0100

    task 4

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,21 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
+    firstName: "Adam",
+    lastName: "Nowak",
     born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+        day: "14",
+        month: "04",
+        year: "1985",
+    },
+};
+
+const currentMonth = new Date().getMonth() + 1;
+const currentDay = new Date().getDate();
+
+if (
+    currentDay === parseInt(user.born.day) &&
+    currentMonth === parseInt(user.born.month)
+) {
+    console.log(`Wszystkiego Najlepszego ${user.firstName}!!!`);
+} else {
+    console.log("Nie masz dziś urodzin :(");
 }

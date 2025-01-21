67cfbb40de2e12ca98a325e199fcd1ecb8db729e
Michał <m.majewski.dm@gmail.com>
2025-01-21 19:06:23 +0100

    task 04 done

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,30 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '21',
+        month: '01',
         year: '1985'
     }
 }
+
+
+function isYourBirthdayToday(user) {
+    const currentDate = new Date();
+    const currentDay = currentDate.getDate();
+    const currentMonth = currentDate.getMonth() + 1;
+
+    return (
+    Number(user.born.day) === currentDay && 
+    Number(user.born.month) === currentMonth);
+}
+
+if(isYourBirthdayToday(user)) {
+    console.log('Wszystkiego najlepszego ' + user['firstName'],  user['lastName']);
+} else {
+    console.log(user['firstName'], user['lastName'] + ' nie masz dziś urodzin.');
+}
+
+// jedna rzecz mnie tylko zastanawia 
+// na początku użyłem parseInt do przekształcenia stringów
+// na liczby aczkolwiek nie chciało działać, a przy uzyciu
+// Number wszystko działa, czemu parseInt nie zadziałało?

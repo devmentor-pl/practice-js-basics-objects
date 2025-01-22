00ee37c78e5b6f415d1f1dcd9efb554020322b4d
Michał <m.majewski.dm@gmail.com>
2025-01-22 09:17:19 +0100

    updated task 01

--- 01/app.js
+++ 01/app.js
@@ -5,7 +5,7 @@ const user = {
     age: 30
 }
 
-console.log(user['firstName']);
-console.log(user['lastName']);
-console.log(user['sex']);
-console.log(user['age']);
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.sex);
+console.log(user.age);

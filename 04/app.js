422529d999e4f80929139a2f3d12e0a50ec796b3
Daria Seńko <twoj@email>
2025-03-25 17:41:05 +0100

    zad 4

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,25 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
+  firstName: 'Adam',
+  lastName: 'Nowak',
+  born: {
+    day: '14',
+    month: '04',
+    year: '1985',
+  },
+};
+
+const date = new Date();
+
+const currDay = date.getDate();
+const currMonth = date.getMonth() + 1;
+
+console.log(currDay, currMonth);
+
+if (
+  parseInt(user.born.day) === currDay &&
+  parseInt(user.born.month) === currMonth
+) {
+  console.log('Mam urodziny');
+} else {
+  console.log('Nie mam urodzin');
 }

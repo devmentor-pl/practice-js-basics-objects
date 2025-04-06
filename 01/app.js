a85031ca5feb0182d2765399699b01b07e7748ae
Gabriel Macierzyński <gabrysm97@gmail.com>
2025-04-06 14:09:48 +0200

    solution to task 1

---
+++ 01/app.js
@@ -0,0 +1,8 @@
+const user = {
+    firstName : "Adam",
+    lastName : "Kowalski",
+    sex : "Mężczyzna",
+    age : 43,
+};
+
+console.log("Imię: " + user.firstName + " Nazwisko: " + user.lastName + " płeć: " + user.sex + " wiek: " + user.age);
